e7fda40a402e2bf6c1099dcc3b6145bdca2aef6a
webhao <954293140@qq.com>
2018-05-02 17:04:59 +0800

    first commit

---
+++ src/PrintModal.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Modal } from "antd";
+import PrintSet from "./PrintSet";
+import PrintPreview from "./PrintPreview";
+class PrintModal extends Component {
+  render() {
+    return (
+      <Modal
+        title="打印设置"
+        visible={this.props.visible}
+        onOk={this.props.onOk}
+        onCancel={this.props.onCancel}
+        okText="确定"
+        width="230mm"
+        style={{ top: 20 }}
+      >
+        <div className="print-modal clearfix">
+          <PrintSet {...this.props} />
+          <div className="print-preview-container">
+            <PrintPreview {...this.props} />
+          </div>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default PrintModal;
---
+++ src/PrintPreview.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PageSizeContainer from "./PageSizeContainer";
+const QRCodeContainerMap = {
+  "1X1": 1,
+  "1X2": 2,
+  "2X3": 6,
+  "3X4": 12
+};
+class PrintPreview extends Component {
+  render() {
+    const {
+      composingStyle,
+      allData
+    } = this.props;
+    let result = [];
+    // 切割allData 每个数组data的个数
+    const itemLength = QRCodeContainerMap[composingStyle];
+    for (let i = 0, len = allData.length; i < len; i += itemLength) {
+      result.push(allData.slice(i, i + itemLength));
+    }
+    return result.map((resultItem, index) => {
+      return (
+        <div className={`QR-code-container p${composingStyle}`} key={index}>
+          <PageSizeContainer
+            {...this.props}
+            oneItem={resultItem}
+            itemLength={itemLength}
+          />
+        </div>
+      );
+    });
+  }
+}
+
+export default PrintPreview;
